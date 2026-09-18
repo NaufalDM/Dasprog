84a008560feec8b164bb1ac8691ed8c9d9ad0352
Naufal <naufaldwikimaulana@gmail.com>
2026-09-18 12:45:35 +0700

    Team Comps

---
+++ probE_TeamComps.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+int main()
+{
+    char a,b,c,d,e;
+    scanf(" %c %c %c %c %c",&a ,&b ,&c ,&d , &e);
+
+    switch(a)
+    {
+        case 'P': printf("Ghost "); break;
+        case 'G': printf("Meteor "); break;
+        case 'D': printf("Clay "); break;
+        case 'A': printf("Zupzup "); break;
+        case 'H': printf("Purina "); break;
+        case 'C': printf("Queen "); break;
+        case 'E': printf("Eternal "); break;
+    }
+
+    switch(b)
+    {
+        case 'P': printf("Ghost "); break;
+        case 'G': printf("Meteor "); break;
+        case 'D': printf("Clay "); break;
+        case 'A': printf("Zupzup "); break;
+        case 'H': printf("Purina "); break;
+        case 'C': printf("Queen "); break;
+        case 'E': printf("Eternal "); break;
+    }
+
+    switch(c)
+    {
+        case 'P': printf("Ghost "); break;
+        case 'G': printf("Meteor "); break;
+        case 'D': printf("Clay "); break;
+        case 'A': printf("Zupzup "); break;
+        case 'H': printf("Purina "); break;
+        case 'C': printf("Queen "); break;
+        case 'E': printf("Eternal "); break;
+    }
+
+    switch(d)
+    {
+        case 'P': printf("Ghost "); break;
+        case 'G': printf("Meteor "); break;
+        case 'D': printf("Clay "); break;
+        case 'A': printf("Zupzup "); break;
+        case 'H': printf("Purina "); break;
+        case 'C': printf("Queen "); break;
+        case 'E': printf("Eternal "); break;
+    }
+
+    switch(e)
+    {
+        case 'P': printf("Ghost "); break;
+        case 'G': printf("Meteor "); break;
+        case 'D': printf("Clay "); break;
+        case 'A': printf("Zupzup "); break;
+        case 'H': printf("Purina "); break;
+        case 'C': printf("Queen "); break;
+        case 'E': printf("Eternal "); break;
+    }
+}
