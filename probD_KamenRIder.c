482017a25a3f4fd825acc74b8f609ec0ad7e1bcb
Naufal <naufaldwikimaulana@gmail.com>
2026-09-18 09:45:41 +0700

    Kamen rider (palindrome without loop

---
+++ probD_KamenRIder.c
@@ -0,0 +1,212 @@
+#include<stdio.h>
+int main()
+{
+    long long x;
+    scanf("%lld", &x);
+
+    if(x<10)
+    {
+        printf("Juicy, aku suka jumlah palindrome!");
+    }
+
+    else if (x<100)
+    {
+        if(x/10==x%10)
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<1000)
+    {
+        if(x/100==x%10)
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<10000)
+    {
+        if(x/1000==x%10 && (x/100)%10==(x/10)%10)
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<100000)
+    {
+        if(x/10000==x%10 && (x/1000)%10==(x/10)%10)
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<1000000)
+    {
+        if(x/100000==x%10 && (x/10000)%10==(x/10)%10 && (x/1000)%10==(x/100)%10)
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<10000000)
+    {
+        if(x/1000000==x%10 && (x/100000)%10==(x/10)%10 && (x/10000)%10==(x/100)%10)
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<100000000)
+    {
+        if(x/10000000==x%10 && (x/1000000)%10==(x/10)%10 && (x/100000)%10==(x/100)%10 && (x/10000)%10==(x/1000)%10)
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<1000000000)
+    {
+        if(x/100000000==x%10 && (x/10000000)%10==(x/10)%10 && (x/1000000)%10==(x/100)%10 && (x/100000)%10==(x/1000)%10)
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<10000000000)
+    {
+        if(x/1000000000==x%10 && (x/100000000)%10==(x/10)%10 && (x/10000000)%10==(x/100)%10 && (x/1000000)%10==(x/1000)%10 && (x/100000)%10==(x/10000)%10) 
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<100000000000)
+    {
+        if(x/10000000000==x%10 && (x/1000000000)%10==(x/10)%10 && (x/100000000)%10==(x/100)%10 && (x/10000000)%10==(x/1000)%10 && (x/1000000)%10==(x/10000)%10) 
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<1000000000000)
+    {
+        if(x/100000000000==x%10 && (x/10000000000)%10==(x/10)%10 && (x/1000000000)%10==(x/100)%10 && (x/100000000)%10==(x/1000)%10 && (x/10000000)%10==(x/10000)%10 && (x/1000000)%10==(x/100000)%10) 
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<10000000000000)
+    {
+        if(x/1000000000000==x%10 && (x/100000000000)%10==(x/10)%10 && (x/10000000000)%10==(x/100)%10 && (x/1000000000)%10==(x/1000)%10 && (x/100000000)%10==(x/10000)%10 && (x/10000000)%10==(x/100000)%10) 
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<100000000000000)
+    {
+        if(x/10000000000000==x%10 && (x/1000000000000)%10==(x/10)%10 && (x/100000000000)%10==(x/100)%10 && (x/1000000000)%10==(x/1000)%10 && (x/1000000000)%10==(x/10000)%10 && (x/100000000)%10==(x/100000)%10 && (x/10000000)%10==(x/1000000)%10) 
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<1000000000000000)
+    {
+        if(x/100000000000000==x%10 && (x/10000000000000)%10==(x/10)%10 && (x/1000000000000)%10==(x/100)%10 && (x/10000000000)%10==(x/1000)%10 && (x/10000000000)%10==(x/10000)%10 && (x/1000000000)%10==(x/100000)%10 && (x/100000000)%10==(x/1000000)%10) 
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+    else if (x<10000000000000000)
+    {
+        if(x/1000000000000000==x%10 && (x/100000000000000)%10==(x/10)%10 && (x/10000000000000)%10==(x/100)%10 && (x/100000000000)%10==(x/1000)%10 && (x/100000000000)%10==(x/10000)%10 && (x/10000000000)%10==(x/100000)%10 && (x/1000000000)%10==(x/1000000)%10 && (x/100000000)%10==(x/10000000)%10) 
+        {
+            printf("Juicy, aku suka jumlah palindrome!");
+        }
+        else
+        {
+            printf("Gamauuu!!, mending makan potato chip!!");
+        }
+    }
+
+else
+{
+    if(x/10000000000000000==x%10 &&
+       (x/1000000000000000)%10==(x/10)%10 &&
+       (x/100000000000000)%10==(x/100)%10 &&
+       (x/10000000000000)%10==(x/1000)%10 &&
+       (x/1000000000000)%10==(x/10000)%10 &&
+       (x/100000000000)%10==(x/100000)%10 &&
+       (x/10000000000)%10==(x/1000000)%10 &&
+       (x/1000000000)%10==(x/10000000)%10)
+    {
+        printf("Juicy, aku suka jumlah palindrome!");
+    }
+    else
+    {
+        printf("Gamauuu!!, mending makan potato chip!!");
+    }
+}
+
+    return 0;
+}
